e6350e794e4c656b46b332c8de92f9192a68fc87
hsqianpin <qianpin.lin@handshakes.com.sg>
2021-08-04 17:22:43 +0800

    2845 Change to ##
    It will cross scripts

--- 2021/2845_KeyfieldInternational/2845_KeyfieldInternational_Merge_CRMY(1.1_Testing).sql
+++ 2021/2845_KeyfieldInternational/2845_KeyfieldInternational_Merge_CRMY(1.1_Testing).sql
@@ -36,25 +36,25 @@ SELECT * FROM KETUPAT_FE.DBO.ENTITY WHERE ACTIVE_NAME LIKE '%KEYFIELD INTERNATIO
 	STEPS
 	1. Creates a "duplicate" relationship with @correct_guid based on every existing entity_FROM_guid=@wrong_guid relationship
 		- Assigns a new @new_rel_guid for the created line(s)
-		- Stores into #new_rel
+		- Stores into ##new_rel
 	2. Creates a "duplicate" relationship with @correct_guid based on every existing entity_TO_guid=@wrong_guid relationship
 		- Assigns a new @new_rel_guid for the created line(s)
-		- Stores into #new_rel
-	3. Updates #new_rel to replace @wrong_guid with @correct_guid, adds respective internal_comment
-	4. Creates tmp MODIFICATION table #mod 
+		- Stores into ##new_rel
+	3. Updates ##new_rel to replace @wrong_guid with @correct_guid, adds respective internal_comment
+	4. Creates tmp MODIFICATION table ##mod 
 		- Assigns new MODIFICATION id for newly created NEW_RELATIONSHIP_GUID
-	5. Prepares respective RELATIONSHIP_SOURCE table into tmp table #new_rel_source
+	5. Prepares respective RELATIONSHIP_SOURCE table into tmp table ##new_rel_source
 	6. Assigns new MODIFICATION id for newly created NEW_RELATIONSHIP_SOURCE_GUID in Step 5
-	7. Updates #mod with creation_datetime and creation_user
-	8. Updates #new_rel table with modification_id created in Step 4
-	9. Updates #new_rel_source table with modification_id created in Step 6
+	7. Updates ##mod with creation_datetime and creation_user
+	8. Updates ##new_rel table with modification_id created in Step 4
+	9. Updates ##new_rel_source table with modification_id created in Step 6
 
 */
 --There is no relationship for @wrong_guid
 
 /*	STEP 1 */
 SELECT new_rel_guid = NEWID(), *
-INTO #new_rel
+INTO ##new_rel
 FROM dbo.RELATIONSHIP r
 WHERE entity_from_guid = @wrong_guid
 AND NOT EXISTS (SELECT 1 FROM RELATIONSHIP w  WHERE w.entity_from_guid=@correct_guid 
@@ -64,7 +64,7 @@ AND NOT EXISTS (SELECT 1 FROM RELATIONSHIP w  WHERE w.entity_from_guid=@correct_
 				)
 
 /*	STEP 2 */
-INSERT INTO #new_rel
+INSERT INTO ##new_rel
 SELECT new_rel_guid = NEWID(), *
 FROM dbo.RELATIONSHIP r
 WHERE entity_to_guid = @wrong_guid
@@ -75,7 +75,7 @@ AND NOT EXISTS (SELECT 1 FROM RELATIONSHIP w  WHERE w.entity_to_guid=@correct_gu
 				)
 
 /*	STEP 3 */
-UPDATE #new_rel
+UPDATE ##new_rel
 SET  entity_from_guid	= IIF(entity_from_guid = @wrong_guid, @correct_guid, entity_from_guid)
 	,entity_to_guid		= IIF(entity_to_guid = @wrong_guid, @correct_guid, entity_to_guid)
 	,internal_comment	= CONCAT(internal_comment, char(13), CAST(GETDATE() as DATE),': copied from rel_guid (',relationship_guid,') due to merge in ',IIF(entity_from_guid=@wrong_guid,'entity_from','entity_to'))
@@ -83,39 +83,39 @@ SET  entity_from_guid	= IIF(entity_from_guid = @wrong_guid, @correct_guid, entit
 
 /*	STEP 4 */
 SELECT new_rel_guid, new_mod_id = NEWID(), m.*
-INTO #mod
-FROM #new_rel r
+INTO ##mod
+FROM ##new_rel r
 JOIN dbo.MODIFICATION m on m.id=r.modification_info
 
 /*	STEP 5 */
 SELECT r.new_rel_guid, new_rs_guid = NEWID(), rs.*
-INTO #new_rel_source
+INTO ##new_rel_source
 FROM dbo.RELATIONSHIP_SOURCE rs
-JOIN #new_rel r on r.relationship_guid = rs.relationship_guid
+JOIN ##new_rel r on r.relationship_guid = rs.relationship_guid
 
 /*	STEP 6 */
-INSERT INTO #mod
+INSERT INTO ##mod
 SELECT rs.new_rs_guid,NEWID(), m.*
-FROM #new_rel_source rs
+FROM ##new_rel_source rs
 JOIN dbo.MODIFICATION m on m.id=rs.modification_info
 
 
 /*	STEP 7 */
-UPDATE #mod
+UPDATE ##mod
 SET  creation_datetime  = @correction_datetime
 	,creation_user		= @user_guid
 
 /*	STEP 8 */
 UPDATE r
 SET modification_info = m.new_mod_id
-FROM #new_rel r
-JOIN #mod m on r.new_rel_guid=m.new_rel_guid
+FROM ##new_rel r
+JOIN ##mod m on r.new_rel_guid=m.new_rel_guid
 
 /*	STEP 9 */
 UPDATE rs
 SET modification_info = m.new_mod_id
-FROM #new_rel_source rs
-JOIN #mod m on rs.new_rs_guid=m.new_rel_guid
+FROM ##new_rel_source rs
+JOIN ##mod m on rs.new_rs_guid=m.new_rel_guid
 
 
 ------------------------------------------------------------------------
@@ -126,18 +126,18 @@ SELECT
 	, new_ent_shr_guid = NEWID()
 	, r.internal_comment as copy_rel_comment
 	, a.*
-INTO #entity_shareholding
-FROM #new_rel r
+INTO ##entity_shareholding
+FROM ##new_rel r
 JOIN dbo.ENTITY_SHAREHOLDING_ALL a on a.relationship_guid = r.relationship_guid
 
 
 
 
 ------------------------------------------------------------------------
-SELECT * FROM #mod
-SELECT * FROM #new_rel
-SELECT * FROM #new_rel_source
-SELECT * FROM #entity_shareholding
+SELECT * FROM ##mod
+SELECT * FROM ##new_rel
+SELECT * FROM ##new_rel_source
+SELECT * FROM ##entity_shareholding
 
 
 COMMIT TRANSACTION;  
--- 2021/2845_KeyfieldInternational/2845_KeyfieldInternational_Merge_CRMY(1.2_Patching).sql
+++ 2021/2845_KeyfieldInternational/2845_KeyfieldInternational_Merge_CRMY(1.2_Patching).sql
@@ -29,7 +29,7 @@ DECLARE  @correct_guid uniqueidentifier = '370D4557-C506-43AC-8E24-EB53B5DB71CE'
 -- ======== ENTITY_SHAREHOLDING_ALL ======== --
 INSERT INTO dbo.ENTITY_SHAREHOLDING_ALL (entity_shrhldg_guid, relationship_guid, shr_type_uid, capital_currency, nominal_val, guaranteed_amt, shr_allocated, joint_shareholding, shr_allocated_joint, is_joint, from_date, internal_comment, external_comment, is_active, source_guid)
 SELECT new_ent_shr_guid, new_rel_guid, shr_type_uid, capital_currency, nominal_val, guaranteed_amt, shr_allocated, joint_shareholding, shr_allocated_joint, is_joint, from_date, [copy_rel_comment], external_comment, is_active, source_guid
-FROM #entity_shareholding
+FROM ##entity_shareholding
 
 
 ------------------------------------------------------------------------
@@ -79,24 +79,24 @@ where id IN (SELECT modification_info FROM RELATIONSHIP WHERE entity_from_guid =
 /* commit changes */
 INSERT INTO dbo.MODIFICATION (id, creation_datetime, creation_user, modification_datetime, modification_user, publication_datetime, publication_user)
 SELECT new_mod_id, creation_datetime, creation_user, modification_datetime, modification_user, publication_datetime, publication_user
-FROM #mod
+FROM ##mod
 
 INSERT INTO dbo.RELATIONSHIP(relationship_guid, workflow_state, entity_from_guid, entity_to_guid, relationship_subtype_uid, from_date, to_date, is_past_relationship, modification_info, external_comment, internal_comment, permission_group_guid, active_source_date, DESIGNATION_TYPE)
 SELECT new_rel_guid, workflow_state, entity_from_guid, entity_to_guid, relationship_subtype_uid, from_date, to_date, is_past_relationship, modification_info, external_comment, internal_comment, permission_group_guid, active_source_date, DESIGNATION_TYPE
-FROM #new_rel
+FROM ##new_rel
 
 INSERT INTO dbo.RELATIONSHIP_SOURCE (relationship_source_guid, relationship_guid, source_guid, modification_info, document_location, discovered_start_date, discovered_end_date, discovered_as_past_relationship, comment)
 SELECT new_rs_guid, new_rel_guid, source_guid, modification_info, document_location, discovered_start_date, discovered_end_date, discovered_as_past_relationship, comment
-FROM #new_rel_source
+FROM ##new_rel_source
 
 
 --======================================================
 --CLEAN UP
 --======================================================
-DROP TABLE #mod
-DROP TABLE #new_rel
-DROP TABLE #new_rel_source
-DROP TABLE #entity_shareholding
+DROP TABLE ##mod
+DROP TABLE ##new_rel
+DROP TABLE ##new_rel_source
+DROP TABLE ##entity_shareholding
 
 
 
