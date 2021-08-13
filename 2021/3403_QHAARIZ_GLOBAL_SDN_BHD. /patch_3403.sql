234d4f72325087a839d83203e9ab6ebe3e7e8adb
wangcongheng <72131163+wangcongheng@users.noreply.github.com>
2021-08-13 13:38:34 +0800

    Update patch_3403.sql
    Made the necessary changes by QP

--- 2021/3403_QHAARIZ_GLOBAL_SDN_BHD. /patch_3403.sql
+++ 2021/3403_QHAARIZ_GLOBAL_SDN_BHD. /patch_3403.sql
@@ -9,14 +9,20 @@
 -- Investigation
 Select * from Ketupat_fe.dbo.entity where company_registration_number = '734463T' --QHAARIZ GLOBAL SDN. BHD.
 Select * from Ketupat_fe.dbo.entity where active_name = 'TEE SWEE GUAN'
-
+select * from ketupat_fe.dbo.modification where id = '9304D59F-0CBD-4855-810B-E2ACB19B56CE'
 select * from ketupat_fe.dbo.RELATIONSHIP where entity_to_guid = '01223CB2-DE06-4A78-852E-F76D3FB82611' and entity_from_guid = '1A59AD29-FE94-4032-9102-E6C720038B00'
 
 
 --Patching
 update ketupat_fe.dbo.RELATIONSHIP
-set is_past_relationship = '0'
+set is_past_relationship = '0' , internal_comment = concat(internal_comment,'20210813 Brute force changing is_past_relationship= '0' to align with CTOS report')
 where relationship_guid = '5EB4018B-DB24-4A24-962D-ED6C7A125551'
+
+update ketupat_fe.dbo.modification
+set modification_datetime = GETDATE(), modification_user = 'BDDB8000-DB42-49AF-9937-9DE6E1FF4CC2' ,
+where id = '9304D59F-0CBD-4855-810B-E2ACB19B56CE'
+
+
 
 
 -- CHECKING
